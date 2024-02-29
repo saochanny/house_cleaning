8964b6a4785aca1b02268535b27ed90c5a17f2b4
Channy SAO <saochanny9944@gmail.com>
2024-03-01 00:53:49 +0700

    first commit

---
+++ src/main/java/com/chanty/house/cleaning/HouseCleaningApplication.java
@@ -0,0 +1,13 @@
+package com.chanty.house.cleaning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseCleaningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HouseCleaningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chanty/house/cleaning/HouseCleaningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chanty.house.cleaning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseCleaningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
